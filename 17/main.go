8af12af8d054f736cf4d1f3f1b8ff422a0d6c7a4
Maksim Litvinov <metalim@gmail.com>
2023-12-17 21:57:16 +0200

    combine day 17 part 1 and 2 (still not accepted for input2)

--- 17/main.go
+++ 17/main.go
@@ -63,56 +63,17 @@ type Crucible struct {
 
 func part1(lines []string) {
 	timeStart := time.Now()
-	visited := make(map[PosDir]Loss)
-
-	next := []Crucible{{PosDir{Pos{0, 0}, Right, 0}, 0}}
-	var current []Crucible
-	var exit bool
-	for !exit {
-		exit = true
-		current, next = next, current[:0]
-		for _, cruc := range current {
-			if v, ok := visited[cruc.PosDir]; ok {
-				if v <= cruc.loss {
-					continue
-				}
-			}
-			exit = false
-			visited[cruc.PosDir] = cruc.loss
-
-			for turn := -1; turn <= 1; turn += 1 {
-				newDir := (cruc.dir + Dir(turn) + 4) % 4
-				steps := cruc.steps
-				if turn == 0 {
-					if cruc.steps == 3 {
-						continue
-					}
-				} else {
-					steps = 0
-				}
-				newPosDir := PosDir{Pos{cruc.x + Moves[newDir].x, cruc.y + Moves[newDir].y}, newDir, steps + 1}
-				if newPosDir.x < 0 || newPosDir.x >= len(lines[0]) || newPosDir.y < 0 || newPosDir.y >= len(lines) {
-					continue
-				}
-				cruc := Crucible{newPosDir, cruc.loss + Loss(lines[newPosDir.y][newPosDir.x]-'0')}
-				next = append(next, cruc)
-			}
-		}
-	}
-
-	var minLoss Loss = 1<<63 - 1
-	for k, v := range visited {
-		if k.x == len(lines[0])-1 && k.y == len(lines)-1 {
-			if minLoss > v {
-				minLoss = v
-			}
-		}
-	}
+	minLoss := findMinLoss(lines, 0, 3)
 	fmt.Println("Part 1:", minLoss, "\tin", time.Since(timeStart))
 }
 
 func part2(lines []string) {
 	timeStart := time.Now()
+	minLoss := findMinLoss(lines, 4, 10)
+	fmt.Println("Part 2:", minLoss, "\tin", time.Since(timeStart))
+}
+
+func findMinLoss(lines []string, minSteps, maxSteps int) Loss {
 	visited := make(map[PosDir]Loss)
 
 	next := []Crucible{{PosDir{Pos{0, 0}, Right, 0}, 0}}
@@ -134,11 +95,11 @@ func part2(lines []string) {
 				newDir := (cruc.dir + Dir(turn) + 4) % 4
 				steps := cruc.steps
 				if turn == 0 {
-					if cruc.steps == 10 {
+					if cruc.steps >= maxSteps {
 						continue
 					}
 				} else {
-					if cruc.steps < 4 {
+					if cruc.steps < minSteps {
 						continue
 					}
 					steps = 0
@@ -156,11 +117,11 @@ func part2(lines []string) {
 	var minLoss Loss = 1<<63 - 1
 	for k, v := range visited {
 		if k.x == len(lines[0])-1 && k.y == len(lines)-1 {
-			fmt.Printf("pos: %v, loss: %v\n", k.Pos, v)
+			// fmt.Printf("pos: %v, dir: %s, step: %d, loss: %v\n", k.Pos, k.dir, k.steps, v)
 			if minLoss > v {
 				minLoss = v
 			}
 		}
 	}
-	fmt.Println("Part 2:", minLoss, "\tin", time.Since(timeStart))
+	return minLoss
 }
