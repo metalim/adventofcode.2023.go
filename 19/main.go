ee30cfb5e5d9f858b818071a08efcae9bdca1b67
Maksim Litvinov <metalim@gmail.com>
2023-12-18 20:57:42 +0200

    add stub for day 19

---
+++ 19/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func catch(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go input.txt")
+		os.Exit(1)
+	}
+
+	bs, err := os.ReadFile(os.Args[1])
+	catch(err)
+	lines := strings.Split(string(bs), "\n")
+
+	part1(lines)
+	part2(lines)
+}
+
+func part1(lines []string) {
+	timeStart := time.Now()
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+
+	fmt.Println("Part 1:", "\tin", time.Since(timeStart))
+}
+
+func part2(lines []string) {
+	timeStart := time.Now()
+	for _, line := range lines {
+		_ = line
+	}
+
+	fmt.Println("Part 2:", "\tin", time.Since(timeStart))
+}
